2898154c60c31f4efeb6ab20966e2a97c0718154
X57FI8W9S <frcofre@gmail.com>
2025-06-07 02:28:16 -0400

    Update tp.launch.py

--- src/cofrevillalon_pkg/launch/tp.launch.py
+++ src/cofrevillalon_pkg/launch/tp.launch.py
@@ -1,16 +1,28 @@
-# turtlesim/launch/multisim.launch.py
 import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch_ros.actions import Node
-from launch.actions import LogInfo
+from launch.actions import ExecuteProcess, LogInfo
 
 def generate_launch_description():
-    launch_dir = os.path.join(get_package_share_directory("clase4"), "launch")
+    # Define the path to your RViz configuration file
+    rviz_config_file = '/root/ros2_ws/src/cofrevillalon_pkg/config/config1.rviz'
+
+    # Get the launch directory for logging purposes
+    launch_dir = os.path.join(get_package_share_directory('cofrevillalon_pkg'), 'launch')
+
     return LaunchDescription([
-        Node(namespace= "turtlesim1", package='turtlesim', executable='turtlesim_node', output='screen'),
+        # Action to play the rosbag at half speed
+        ExecuteProcess(
+            cmd=['ros2', 'bag', 'play', 'r2b_galileo2', '-r0.5'],
+            output='screen'
+        ),
 
-        Node(namespace= "turtlesim2", package='turtlesim', executable='turtlesim_node', output='screen'),
+        # Action to launch RViz2 with the specified configuration
+        ExecuteProcess(
+            cmd=['ros2', 'run', 'rviz2', 'rviz2', '-d', rviz_config_file],
+            output='screen'
+        ),
 
-        LogInfo(msg=f"Usando mi launchfile del paquete de ROS de la clase 4 el directorio: {launch_dir}"),
+        # Log a message indicating which launch file is being used
+        LogInfo(msg=f"Running launch file from: {launch_dir}"),
     ])
